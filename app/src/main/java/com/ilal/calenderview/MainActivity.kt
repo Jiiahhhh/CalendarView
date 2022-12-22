e9fedb84a3b5567b7ad2b8f2395726cd29cbf3c6
Jiiahhhh <ijlalnurhadi01@gmail.com>
2022-12-22 21:38:40 +0800

    Calendar View Works

--- app/src/main/java/com/ilal/calenderview/MainActivity.kt
+++ app/src/main/java/com/ilal/calenderview/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.ilal.calenderview
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ilal.calenderview.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnKalendar.setOnClickListener {
+            setDate()
+        }
+        setToolbar()
     }
+
+    private fun setToolbar(){
+        supportActionBar?.title = "Kalender"
+        supportActionBar?.subtitle = "Tutorial mencoba berbagai fitur"
+    }
+
+    private fun setDate(){
+        val datePicker = Calendar.getInstance()
+        val date = DatePickerDialog.OnDateSetListener {
+                view, year, month, day ->
+            datePicker[Calendar.YEAR] = year
+            datePicker[Calendar.MONTH] = month
+            datePicker[Calendar.DAY_OF_MONTH] = day
+            val dateFormat = "dd-MMMM-yyyy"
+            val simpleDateFormat = SimpleDateFormat(dateFormat, Locale.getDefault())
+            binding.text.text = simpleDateFormat.format(datePicker.time)
+        }
+        DatePickerDialog(
+            this@MainActivity,date,
+            datePicker[Calendar.YEAR],
+            datePicker[Calendar.MONTH],
+            datePicker[Calendar.DAY_OF_MONTH]
+        ).show()
+    }
+
 }
